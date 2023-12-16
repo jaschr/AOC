04e7fc1cd23688d2973958a5971b55de5515693b
Jacob Schroeder <j.alexander.sch@gmail.com>
2023-12-16 03:12:29 -0500

    part2 boiler

--- advent/lib/day02.ex
+++ advent/lib/day02.ex
@@ -1,9 +1,23 @@
-
 defmodule Advent.Day02.Part1 do
   @moduledoc """
   # Documentation for `Advent.Day02.Part1`.
 
   Solution for *Day 02 Part 1*.
+  """
+  use Advent
+
+  def solution(path) do
+    path
+    |> read_lines()
+  end
+
+end
+
+defmodule Advent.Day02.Part2 do
+  @moduledoc """
+  # Documentation for `Advent.Day02.Part2`.
+
+  Solution for *Day 02 Part 2*.
   """
   use Advent
 
