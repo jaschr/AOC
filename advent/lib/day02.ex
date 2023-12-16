59e96262a45775b6f2c9d90591bba0212dce4fe1
Jacob Schroeder <j.alexander.sch@gmail.com>
2023-12-16 03:04:58 -0500

    day2 part1 boilerplate

---
+++ advent/lib/day02.ex
@@ -0,0 +1,15 @@
+
+defmodule Advent.Day02.Part1 do
+  @moduledoc """
+  # Documentation for `Advent.Day02.Part1`.
+
+  Solution for *Day 02 Part 1*.
+  """
+  use Advent
+
+  def solution(path) do
+    path
+    |> read_lines()
+  end
+
+end
