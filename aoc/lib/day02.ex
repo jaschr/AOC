a3a7785a301c5bf2db97f272bc15b18d814b7dc9
Jacob Schroeder <j.alexander.sch@gmail.com>
2023-12-24 00:57:35 -0500

    day 2 part 1 + fixed @spec

---
+++ aoc/lib/day02.ex
@@ -0,0 +1,51 @@
+defmodule AOC.Day02.Part1 do
+  @moduledoc """
+  Documentation for `AOC.Day02.Part1`
+
+  Solves the 2023 Advent of Code Day 2 Part 1 challenge.
+  """
+
+  use AOC
+
+  @reds 12
+  @greens 13
+  @blues 14
+
+  def solve(file) do
+    file
+    |> read_file_to_list()
+    |> trim_newlines?()
+    |> Enum.map(&parse_game/1)
+    |> Enum.filter(fn {_id, [red: red, green: green, blue: blue]} ->
+      red <= @reds && green <= @greens && blue <= @blues
+    end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sum()
+  end
+
+  @spec parse_game(String.t()) :: tuple()
+  defp parse_game("Game " <> game) do
+    [id, round] = game |> String.split(": ")
+
+    parsed_round =
+      round
+      |> String.split("; ")
+      |> Enum.map(&String.split(&1, ", "))
+      |> Enum.map(fn reach -> Enum.map(reach, &String.split(&1, " ")) end)
+      |> Enum.map(fn reach ->
+        Enum.map(reach, fn [count, colour] -> {String.to_atom(colour), String.to_integer(count)} end)
+      end)
+
+    {String.to_integer(id), [red: max_by_colour(parsed_round, :red), green: max_by_colour(parsed_round, :green), blue: max_by_colour(parsed_round, :blue)]}
+  end
+
+  @spec max_by_colour([any()], atom()) :: any()
+  defp max_by_colour(round, colour) when colour in [:red, :green, :blue] do
+    round
+    |> Enum.max_by(&Keyword.get(&1, colour, 0))
+    |> Keyword.get(colour, 0)
+  end
+
+  defp max_by_colour(_round, _colour), do: raise("Not a valid colour")
+
+end
